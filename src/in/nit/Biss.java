d0d4b65084d71ebe10edb06d2d294333bdcc1dfe
DELL <DELL@DESKTOP-LPQNOHA>
2021-02-17 02:03:47 +0530

    Hii i have added one method in biss class

--- src/in/nit/Biss.java
+++ src/in/nit/Biss.java
@@ -1,9 +1,24 @@
 package in.nit;
 
-public class Biss {
+import java.util.Scanner;
 
+public class Biss {
+public static void m1() {
+	System.out.println("enter the value of a and b");
+	
+	Scanner scn=new Scanner(System.in);
+	int a1=scn.nextInt();
+	int a2=scn.nextInt();
+	
+	System.out.println("swap the value of a and b");
+	a1=a1+a2;
+	a2=a1-a2;
+	a1=a1-a2;
+	System.out.println("After swaping the value a  and b is \n"+a1 +" \n"+a2);
+}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	m1();
 
 	}
 
